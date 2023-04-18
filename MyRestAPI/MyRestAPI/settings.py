9e1ecb7d32f61991620f59784d93afe1848eed25
Meshkat Uddin Ahammed <meshkatuddinahammed@outlook.com>
2023-04-18 21:58:16 +0600

    Created some orders

--- MyRestAPI/MyRestAPI/settings.py
+++ MyRestAPI/MyRestAPI/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -46,6 +47,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60)
 }
 
 ROOT_URLCONF = 'MyRestAPI.urls'
